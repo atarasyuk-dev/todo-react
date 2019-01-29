6440fc32cc32238fb9a0e79487f80b488f82fbcc
atarasyuk <anton.tarasyuk@creditexpress.ua>
2019-01-29 10:02:55 +0200

    add bootstrap style design app

--- src/components/search-panel.js
+++ src/components/search-panel.js
@@ -1,7 +1,11 @@
 import React from "react";
 
+import './search-panel.css';
+
 const SearchPanel = () => {
-    return <input placeholder="search"/>;
+    return (
+        <input type="text" placeholder="type to search" className="form-control search-input"/>
+    );
 };
 
 export default SearchPanel;
